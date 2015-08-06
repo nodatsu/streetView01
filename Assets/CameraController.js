b2f13f024e627d216de6ef88dfdc410a95abab65
角田 均 <tsunoda@aomori-u.ac.jp>
2015-08-06 15:19:20 +0900

    camera controll script, plane image update script(completed)

---
+++ Assets/CameraController.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKey(KeyCode.UpArrow)) {
+		this.transform.Rotate(Vector3.right, -1);
+	}
+	if (Input.GetKey(KeyCode.DownArrow)) {
+		this.transform.Rotate(Vector3.right, 1);
+	}
+	if (Input.GetKey(KeyCode.LeftArrow)) {
+		this.transform.Rotate(Vector3.up, -1, Space.World);
+	}
+	if (Input.GetKey(KeyCode.RightArrow)) {
+		this.transform.Rotate(Vector3.up, 1, Space.World);
+	}
+	if (Input.GetKey(KeyCode.Space)) {
+		this.transform.rotation = Quaternion.Euler(0, 0, 0);
+	}
+
+}
