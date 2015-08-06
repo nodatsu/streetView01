ff207ff490d0da8ea21f98d420de8bc80a9cc946
角田 均 <tsunoda@aomori-u.ac.jp>
2015-08-06 18:25:20 +0900

    modify size and animation

--- Assets/UnityChan.js
+++ Assets/UnityChan.js
@@ -2,27 +2,38 @@
 
 var rr: float = 5.0;
 var theta: float = 0.0;
-var speed: float = 0.0;
+
+var motion: int = 0;
 
 function Start () {
-
+	StartCoroutine(Loop());
 }
 
 function Update () {
 	transform.position.x = rr * Mathf.Sin(theta);
 	transform.position.z = rr * Mathf.Cos(theta);
 	transform.rotation = Quaternion.Euler(0, theta * Mathf.Rad2Deg + 90, 0);
-	theta += speed;
+
+	switch (motion) {
+		case 0:
+			this.GetComponent(Animator).SetBool("isRunning", false);
+			theta += 0.003;
+			break;
+		case 1:
+			this.GetComponent(Animator).SetBool("isRunning", true);
+			theta += 0.012;
+			break;
+	}
+
 	if (theta >= Mathf.PI * 4) {
 		theta -= Mathf.PI * 4;
 	}
+}
 
-	if (theta < Mathf.PI * 2) {
-		this.GetComponent(Animator).SetBool("isRunning", false);
-		speed = 0.003;
-	}
-	else {
-		this.GetComponent(Animator).SetBool("isRunning", true);
-		speed = 0.012;
+function Loop () {
+	while (true) {
+		yield WaitForSeconds(5);
+		motion += 1;
+		motion = motion % 2;
 	}
 }
