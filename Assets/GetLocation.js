cc6917581771ce09fe8ec94abc2ad4aa9136d60e
角田 均 <tsunoda@aomori-u.ac.jp>
2015-08-06 16:08:24 +0900

    use location

---
+++ Assets/GetLocation.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+public var longitude: double;
+public var latitude: double;
+public var done: boolean = false;
+
+function Start () {
+	// First, check if user has location service enabled
+	if (!Input.location.isEnabledByUser)
+		return;
+
+	// Start service before querying location
+	Input.location.Start ();
+
+	// Wait until service initializes
+	var maxWait : int = 60;
+	while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+		yield WaitForSeconds (1);
+		maxWait--;
+	}
+	// Service didn't initialize in maxWait seconds
+	if (maxWait < 1) {
+		print ("Timed out");
+		return;
+	}
+
+	// Connection has failed
+	if (Input.location.status == LocationServiceStatus.Failed) {
+		print ("Unable to determine device location");
+		return;
+	}
+	// Access granted and location value could be retrieved
+	else {
+		longitude = Input.location.lastData.longitude;
+		latitude = Input.location.lastData.latitude;
+		done = true;
+	}
+
+	// Stop service if there is no need to query location updates continuously
+	Input.location.Stop ();
+}
+
+function Update () {
+
+}
--- Assets/PlaneImageUpdator.js
+++ Assets/PlaneImageUpdator.js
@@ -15,18 +15,32 @@ function updateTexture () {
 	var width: int = 640;
 	var height: int = 480;
 
-	// Sample
-//	var longitude: double = 139.667431;
-//	var latitude: double = 35.697408;
-	// SHINMACHI
-	var longitude: double = 140.741377;
-	var latitude: double = 40.826271;
+	var longitude: double;
+	var latitude: double;
 
-	var url: String = "http://maps.googleapis.com/maps/api/streetview?" + "size=" + width + "x" + height + "&location=" + latitude + "," + longitude + "&heading=" + heading + "&pitch=" + pitch + "&fov=90&sensor=false";
-	print(url);
+	if (Application.platform == RuntimePlatform.Android) {
+		var loc: GetLocation = this.transform.parent.GetComponent.<GetLocation>();
+
+		while (!loc.done) {
+			yield;
+		}
+        
+		longitude = loc.longitude;
+		latitude = loc.latitude;
+	}
+	else if (Application.platform == RuntimePlatform.OSXEditor) {
+		// Sample
+		// var longitude: double = 139.667431;
+		// var latitude: double = 35.697408;
+		// SHINMACHI
+		longitude = 140.741377;
+		latitude = 40.826271;
+	}
 	
+	var url: String = "http://maps.googleapis.com/maps/api/streetview?" + "size=" + width + "x" + height + "&location=" + latitude + "," + longitude + "&heading=" + heading + "&pitch=" + pitch + "&fov=90&sensor=false";	
 	var www: WWW = new WWW(url);
 
 	yield www;
-	this.renderer.material.mainTexture = www.texture;
+
+	this.GetComponent.<Renderer>().material.mainTexture = www.texture;
 }
